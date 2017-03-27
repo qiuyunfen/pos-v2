5cb7d86cf5c4a4255266a724840a76e7e66ffc6f
qiuyunfen <1150382430@qq.com>
2017-03-27 21:21:32 +0800

    implements printStr

--- spec/pos-v2/posSpec.js
+++ spec/pos-v2/posSpec.js
@@ -1,3 +1,4 @@
+
 describe('pos', function() {
 
     let Pos = require('../../lib/pos/pos');
@@ -61,7 +62,48 @@ describe('pos', function() {
         expect(receipt).toEqual({
             save: 3,
             price: 18,
-            receipt: [{name: '雪碧', unit: '瓶', price: 3.00, count: 7, save: 3, price: 18}]
+            receipt: [{name: '雪碧', unit: '瓶', price: 3.00, count: 7, save: 3, sum: 18}]
         })
     })
+
+    it('should return Receipt String', function() {
+       const tags = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2.5',
+            'ITEM000005',
+            'ITEM000005-2',
+        ];
+
+        let dateDigitToString = num => (num < 10 ? `0${num}` : num);
+
+        //spyOn(console, 'log');
+
+        Pos.printReceipt(tags);
+
+        const currentDate = new Date(),
+        year = dateDigitToString(currentDate.getFullYear()),
+        month = dateDigitToString(currentDate.getMonth() + 1),
+        date = dateDigitToString(currentDate.getDate()),
+        hour = dateDigitToString(currentDate.getHours()),
+        minute = dateDigitToString(currentDate.getMinutes()),
+        second = dateDigitToString(currentDate.getSeconds()),
+        formattedDateString = `${year}年${month}月${date}日 ${hour}:${minute}:${second}`;
+
+        const expectText = `***<没钱赚商店>收据***
+打印时间：${formattedDateString}
+----------------------
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：58.50(元)
+节省：7.50(元)
+**********************`;
+
+    expect(console.log).toHaveBeenCalledWith(expectText);
+    });
 });
