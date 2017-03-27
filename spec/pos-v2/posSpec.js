3d6a3199bb679d6015c7ce0b4085f9403237e95c
qiuyunfen <1150382430@qq.com>
2017-03-27 15:43:15 +0800

    implement countBarcodes

--- spec/pos-v2/posSpec.js
+++ spec/pos-v2/posSpec.js
@@ -1,18 +1,38 @@
 describe('pos', function() {
 
-    let analysisBarcode = require('../../lib/pos/pos');
+    let Pos = require('../../lib/pos/pos');
 
     it('should return object 1 when barcodes no count', function() {
-        let aBarcodes = analysisBarcode(['ITEM000001']);
+        let aBarcodes = Pos.analysisBarcodes(['ITEM000001']);
         expect(aBarcodes).toEqual([
             {barcode: 'ITEM000001', count: 1}
         ]);
     })
 
     it('should return object 1 when barcodes has count', function() {
-        let aBarcodes = analysisBarcode(['ITEM000001-3']);
+        let aBarcodes = Pos.analysisBarcodes(['ITEM000001-3']);
         expect(aBarcodes).toEqual([
             {barcode: 'ITEM000001', count: 3}
+        ])
+    })
+
+    it('shoule return object with count when barcodes no count', function() {
+        let barcodes = Pos.countBarcodes([
+            {barcode: 'ITEM000001', count: 1},
+            {barcode: 'ITEM000001', count: 1}
+        ]);
+        expect(barcodes).toEqual([
+            {barcode: 'ITEM000001', count: 2}
+        ]);
+    })
+
+    it('should return object with count when barcodes has count', function() {
+        let barcodes = Pos.countBarcodes([
+            {barcode: 'ITEM000001', count: 3},
+            {barcode: 'ITEM000001', count: 4}
+        ]);
+        expect(barcodes).toEqual([
+            {barcode: 'ITEM000001', count: 7}
         ])
     })
 });
