0b48bdeae0255c316ba65e956d664752592fb094
qiuyunfen <1150382430@qq.com>
2017-03-27 11:47:03 +0800

    implement analysisBarcodes when barcodes include no count barcode

---
+++ spec/pos-v2/posSpec.js
@@ -0,0 +1,18 @@
+describe('pos', function() {
+
+    let analysisBarcode = require('../../lib/pos/pos');
+
+    it('should return object 1 when barcodes no count', function() {
+        let aBarcodes = analysisBarcode(['ITEM000001']);
+        expect(aBarcodes).toEqual([
+            {barcode: 'ITEM000001', count: 1}
+        ]);
+    })
+
+    // it('should return object 1 when barcodes has count', function() {
+    //     let aBarcodes = analysisBarcode('ITEM000001-3');
+    //     expect(aBarcodes).toEqual([
+    //         {barcode: 'ITEM000001', count: 3}
+    //     ])
+    // })
+});
