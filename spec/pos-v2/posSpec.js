3bd7951a3e1d31fe655535e961853028fcc0a847
qiuyunfen <1150382430@qq.com>
2017-03-28 11:00:14 +0800

    add gitignore

--- spec/pos-v2/posSpec.js
+++ spec/pos-v2/posSpec.js
@@ -3,14 +3,14 @@ describe('pos', function() {
 
     let Pos = require('../../lib/pos/pos');
 
-    it('should return object 1 when barcodes no count', function() {
+    it('should return barcodes when barcodes no count', function() {
         let aBarcodes = Pos.analysisBarcodes(['ITEM000001']);
         expect(aBarcodes).toEqual([
             {barcode: 'ITEM000001', count: 1}
         ]);
     })
 
-    it('should return object 1 when barcodes has count', function() {
+    it('should return barcodes when barcodes has count', function() {
         let aBarcodes = Pos.analysisBarcodes(['ITEM000001-3']);
         expect(aBarcodes).toEqual([
             {barcode: 'ITEM000001', count: 3}
